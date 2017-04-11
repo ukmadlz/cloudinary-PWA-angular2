8d6ad73a6ab83d58e33a3eded24bd72a821576f2
Mike Elsmore <mike@elsmore.me>
2017-04-11 21:23:22 +0100

    Let's start from the beginning

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { ViewComponent } from './view/view.component';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'view/:publicId', component: ViewComponent }
-];
